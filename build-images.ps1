541ef4b313689a5cc96ed04a6732a02a034c44a3
MichaelWorkineh_git <micwor96@gmail.com>
2025-12-25 22:03:21 +0300

    chore: Add build and setup scripts

---
+++ build-images.ps1
@@ -0,0 +1,10 @@
+# Build all services
+docker build -t api-gateway:latest -f ./services/api-gateway/Dockerfile ./services
+docker build -t auth-service:latest -f ./services/auth-service/Dockerfile ./services
+docker build -t document-service:latest -f ./services/document-service/Dockerfile ./services
+docker build -t collaboration-service:latest -f ./services/collaboration-service/Dockerfile ./services
+docker build -t presence-service:latest -f ./services/presence-service/Dockerfile ./services
+docker build -t storage-service:latest -f ./services/storage-service/Dockerfile ./services
+docker build -t reconciliation-service:latest -f ./services/reconciliation-service/Dockerfile ./services
+docker build -t failover-service:latest -f ./services/failover-service/Dockerfile ./services
+# docker build -t client:latest ./client
